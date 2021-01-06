793b3fe184e1bfa4f92b0610833305352a0c86dc
piotrpiedel <piotrpiedel96@gmail.com>
2021-01-06 21:45:00 +0100

    Change datasets and udate config of them

--- src/main/java/uek/mh/VRPRunner.java
+++ src/main/java/uek/mh/VRPRunner.java
@@ -8,20 +8,20 @@ import java.io.IOException;
 public class VRPRunner {
 
     public static void main(String[] args) throws IOException {
-        VrpData vrpDataForFirstRun = DataFromFileConverterToInitialVrpData.convert("datasets/supported_datasets/Golden_20.vrp");
-        new TabuSearchAlgorithm(vrpDataForFirstRun)
-                .run()
-                .printOnlyCalculatedCost();
-
-        VrpData vrpDataForSecondRun = DataFromFileConverterToInitialVrpData.convert("datasets/supported_datasets/Golden_19.vrp");
-        new TabuSearchAlgorithm(vrpDataForSecondRun)
-                .run()
-                .printOnlyCalculatedCost();
-
-        VrpData vrpDataForThirdRun = DataFromFileConverterToInitialVrpData.convert("datasets/supported_datasets/Golden_18.vrp");
-        new TabuSearchAlgorithm(vrpDataForThirdRun)
-                .run()
-                .printOnlyCalculatedCost();
+//        VrpData vrpDataForFirstRun = DataFromFileConverterToInitialVrpData.convert("datasets/supported_datasets/Golden_20.vrp");
+//        new TabuSearchAlgorithm(vrpDataForFirstRun)
+//                .run()
+//                .printOnlyCalculatedCost();
+//
+//        VrpData vrpDataForSecondRun = DataFromFileConverterToInitialVrpData.convert("datasets/supported_datasets/Golden_19.vrp");
+//        new TabuSearchAlgorithm(vrpDataForSecondRun)
+//                .run()
+//                .printOnlyCalculatedCost();
+//
+//        VrpData vrpDataForThirdRun = DataFromFileConverterToInitialVrpData.convert("datasets/supported_datasets/Golden_18.vrp");
+//        new TabuSearchAlgorithm(vrpDataForThirdRun)
+//                .run()
+//                .printOnlyCalculatedCost();
 
         VrpData vrpDataPolandCitiesFromMhProject = DataFromFileConverterToInitialVrpData.convert("datasets/supported_datasets/Mhprojekt.vrp");
         new TabuSearchAlgorithm(vrpDataPolandCitiesFromMhProject)
